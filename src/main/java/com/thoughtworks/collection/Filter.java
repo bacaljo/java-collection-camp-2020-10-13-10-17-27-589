8c1b961cf2d146615925865430f844859e81d1a0
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-14 13:46:25 +0800

    [Filter] [John Emmanuel] feature: complete logic of filterEven

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,18 +1,22 @@
 package com.thoughtworks.collection;
 
 import org.apache.commons.lang3.NotImplementedException;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream()
+                .filter(integer -> integer % 2 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
